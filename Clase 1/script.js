92048dff4c6c49c252fd99a1d93c5dfe540c4cc8
guiarango <guiarango@hotmail.com>
2023-02-13 09:19:06 -0500

    Primera clase

---
+++ Clase 1/script.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Ingrese su nombre");
+
+// alert("Bienvenido " + nombre + " a la pagina de Tennis");
+
+const mensajeBienvenida = "Bienvenido " + nombre + " a la pagina de Tennis";
