0bd1768172455533d9ba1a2bddfaa1e15ec49698
guiarango <guiarango@hotmail.com>
2023-03-15 09:07:09 -0500

    Se agrega ejercicio DOM

---
+++ Ejercicio Manipulacion DOM/script.js
@@ -0,0 +1,26 @@
+const informacion = {
+  cami: {
+    nombre: "Camilo",
+    apellido: "Muñoz",
+    estatura: 1.76,
+    colorFavorito: "Verde",
+  },
+  juli: {
+    nombre: "Juliana",
+    apellido: "Vélez",
+    estatura: 1.65,
+    colorFavorito: "Gris",
+  },
+  jack: {
+    nombre: "Jackson",
+    apellido: "Restrepo",
+    estatura: 1.63,
+    colorFavorito: "Azul",
+  },
+  guille: {
+    nombre: "Guillermo",
+    apellido: "Arango",
+    estatura: 1.8,
+    colorFavorito: "Blanco",
+  },
+};
