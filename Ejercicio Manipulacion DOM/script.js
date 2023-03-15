78b28acd908075b2c11bcb8a5b0a57dd63abd513
guiarango <guiarango@hotmail.com>
2023-03-15 10:22:49 -0500

    Se finaliza el  ejercicio DOM

--- Ejercicio Manipulacion DOM/script.js
+++ Ejercicio Manipulacion DOM/script.js
@@ -24,3 +24,41 @@ const informacion = {
     colorFavorito: "Blanco",
   },
 };
+
+//Solucion 1
+// const aElements = document.querySelectorAll("a");
+// const nombre = document.querySelector(".infoUsuario h1");
+// const estatura = document.querySelector(".infoUsuario .estatura");
+// const color = document.querySelector(".infoUsuario .color");
+
+// aElements.forEach((a) => {
+//   a.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     const nombreElemetoA = event.target.innerText;
+//     const nombreMinuscula = nombreElemetoA.toLowerCase();
+//     const infoUsuarioSeleccionado = informacion[nombreMinuscula];
+
+//     nombre.innerText = `${infoUsuarioSeleccionado.nombre} ${infoUsuarioSeleccionado.apellido}`;
+//     estatura.innerText = infoUsuarioSeleccionado.estatura;
+//     color.innerText = infoUsuarioSeleccionado.colorFavorito;
+//   });
+// });
+
+//Solucion 2
+const ul = document.querySelector("ul");
+const nombre = document.querySelector(".infoUsuario h1");
+const estatura = document.querySelector(".infoUsuario .estatura");
+const color = document.querySelector(".infoUsuario .color");
+
+ul.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.localName === "a") {
+    const nombreElemetoA = target.innerText;
+    const nombreMinuscula = nombreElemetoA.toLowerCase();
+    const infoUsuarioSeleccionado = informacion[nombreMinuscula];
+
+    nombre.innerText = `${infoUsuarioSeleccionado.nombre} ${infoUsuarioSeleccionado.apellido}`;
+    estatura.innerText = infoUsuarioSeleccionado.estatura;
+    color.innerText = infoUsuarioSeleccionado.colorFavorito;
+  }
+});
